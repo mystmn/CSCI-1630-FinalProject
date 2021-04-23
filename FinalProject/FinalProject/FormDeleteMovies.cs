92595c8ebb8a5a4557b923a1bb5b7e56a355905a
pcame <pcameron5@gmail.com>
2021-04-22 22:09:46 -0700

    saving locally

--- FinalProject/FinalProject/FormDeleteMovies.cs
+++ FinalProject/FinalProject/FormDeleteMovies.cs
@@ -12,9 +12,26 @@ namespace FinalProject
 {
     public partial class FormDeleteMovies : Form
     {
+        Messages errors = new Messages();
+
         public FormDeleteMovies()
         {
             InitializeComponent();
+        }
+
+        private void buttonFindMovie_Click(object sender, EventArgs e)
+        {
+            DB_Conn conn = new DB_Conn();
+
+            if (!string.IsNullOrEmpty(textBoxBoxOffice.Text))
+            {
+                errors.input(Messages.validation.Title);
+            }
+            
+            conn.findingTitle(textBoxMovieTitle.Text);
+            List<Movie> x = conn.FoundTitle;
+
+            textBoxDirector.Text = string.Join(" ", x[0]);
         }
     }
 }
